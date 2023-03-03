1f29780a955488ed0fa00f70b565c2a36e828161
Evelina <eve1ina@list.ru>
2023-03-03 14:06:52 +0300

    Добавлены фрагмент FirstScreen в Activity

--- app/src/main/java/com/example/pr3/MainActivity.java
+++ app/src/main/java/com/example/pr3/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.pr3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment firstScreen = new FirstScreen();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.first_screen, firstScreen);
+        fragmentTransaction.commit();
     }
 }
