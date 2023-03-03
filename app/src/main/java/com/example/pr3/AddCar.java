bb62c78662242f1b878790060858614f7ee85209
Evelina <eve1ina@list.ru>
2023-03-03 17:22:27 +0300

    Добавлены Toast во фрагментах

--- app/src/main/java/com/example/pr3/AddCar.java
+++ app/src/main/java/com/example/pr3/AddCar.java
@@ -1,5 +1,6 @@
 package com.example.pr3;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class AddCar extends Fragment {
 
@@ -30,19 +32,27 @@ public class AddCar extends Fragment {
         if (getArguments() != null) {
 
         }
+        Toast.makeText(getActivity(), "onCreateAddCar", Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Toast.makeText(getActivity(), "onAttachAddCar", Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.fragment_add_car, container, false);
-
+        View v = inflater.inflate(R.layout.fragment_add_car, container, false);
+        Toast.makeText(getActivity(), "onCreateViewAddCar", Toast.LENGTH_SHORT).show();
         backButton = (Button) v.findViewById(R.id.GoBackButton);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "Button click in FirstScreen");
+                Log.d(TAG, "Button click in AddCar");
                 if (savedInstanceState == null) {
                     getActivity().getSupportFragmentManager().beginTransaction()
                             .replace(R.id.container, FirstScreen.newInstance())
@@ -51,5 +61,36 @@ public class AddCar extends Fragment {
             }
         });
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Toast.makeText(getActivity(), "onResumeAddCar", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Toast.makeText(getActivity(), "onPauseAddCar", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Toast.makeText(getActivity(), "onDestroyViewAddCar", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Toast.makeText(getActivity(), "onDetachAddCar", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getActivity(), "onDestroyAddCar", Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/example/pr3/FirstScreen.java
+++ app/src/main/java/com/example/pr3/FirstScreen.java
@@ -1,5 +1,6 @@
 package com.example.pr3;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstScreen extends Fragment {
 
@@ -26,11 +28,26 @@ public class FirstScreen extends Fragment {
         return fragment;
     }
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+        Toast.makeText(getActivity(), "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Toast.makeText(getActivity(), "onAttach", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v;
         v = inflater.inflate(R.layout.fragment_first_screen, container, false);
-
+        Toast.makeText(getActivity(), "onCreateView", Toast.LENGTH_SHORT).show();
 
         addCarButton = (Button) v.findViewById(R.id.add_car_button);
 
@@ -53,4 +70,35 @@ public class FirstScreen extends Fragment {
         });
         return v;
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Toast.makeText(getActivity(), "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Toast.makeText(getActivity(), "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Toast.makeText(getActivity(), "onDestroyView", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Toast.makeText(getActivity(), "onDetach", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getActivity(), "onDestroy", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
